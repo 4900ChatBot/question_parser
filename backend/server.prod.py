f02c3686edca17ac53ecf408eed449f7ad947822
Kevin Wong <kewong@transperfect.com>
2019-05-08 15:20:33 -0400

    heroku port setup

--- backend/server.prod.py
+++ backend/server.prod.py
@@ -1,5 +1,6 @@
 from rasa_core.agent import Agent
 from rasa_core.interpreter import RasaNLUInterpreter
+import os
 import time 
 import json
 
@@ -7,6 +8,7 @@ interpreter = RasaNLUInterpreter('models/current/get_intent')
 agent = Agent.load('models/dialogue_chatbot', interpreter=interpreter)
 
 from flask import Flask
+
 app = Flask(__name__, static_url_path='')
 @app.route('/api/ask/<question>')
 def ask(question):
@@ -17,4 +19,5 @@ def ask(question):
 def root():
     return app.send_static_file('index.html')
 
-app.run()
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
